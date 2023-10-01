2baa201ac641a326cc1ff6cd50cc6ddb41792585
PHS00 <hyunsoo159487@naver.com>
2023-10-01 19:50:42 +0900

    feat : (#15) 투표 옵션 repo 에서 각 투표 옵션 리스트 찾는 쿼리 추가

--- src/main/java/com/kakao/golajuma/vote/infra/repository/OptionJPARepository.java
+++ src/main/java/com/kakao/golajuma/vote/infra/repository/OptionJPARepository.java
@@ -1,6 +1,13 @@
 package com.kakao.golajuma.vote.infra.repository;
 
 import com.kakao.golajuma.vote.infra.entity.OptionEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface OptionJPARepository extends JpaRepository<OptionEntity, Integer> {}
+public interface OptionJPARepository extends JpaRepository<OptionEntity, Integer> {
+
+	@Query("select o from OptionEntity where o.voteId = :voteId")
+	List<OptionEntity> findAllByVoteId(@Param("voteID") long voteId);
+}
