ec8ca8861f326196c4b1f1df548a4eb7e7c1b907
jinwooseok <jinus7949@naver.com>
2023-10-08 00:52:05 +0900

    refactor : (#29) 개별 entity는 optional로 바꿈. optional의 메서드를 사용

--- src/main/java/com/kakao/golajuma/comment/infra/repository/CommentRepository.java
+++ src/main/java/com/kakao/golajuma/comment/infra/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.kakao.golajuma.comment.infra.repository;
 
 import com.kakao.golajuma.comment.infra.entity.CommentEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
 	@Query("Select c from CommentEntity c where c.id = :commentId")
-	CommentEntity findById(@Param("commentId") long commentId);
+	Optional<CommentEntity> findById(@Param("commentId") Long commentId);
 
 	@Query("select c from CommentEntity c where c.voteId = :voteId")
 	List<CommentEntity> findByVoteId(@Param("voteId") long voteId);
