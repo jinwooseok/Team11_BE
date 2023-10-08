c21f78c28c3ce65d8d4bd484ee54030ad5015626
PHS00 <hyunsoo159487@naver.com>
2023-10-09 00:49:11 +0900

    feat : option custom validation 추가

--- src/main/java/com/kakao/golajuma/vote/domain/service/CreateVoteService.java
+++ src/main/java/com/kakao/golajuma/vote/domain/service/CreateVoteService.java
@@ -1,8 +1,5 @@
 package com.kakao.golajuma.vote.domain.service;
 
-import com.kakao.golajuma.vote.domain.exception.NullException;
-import com.kakao.golajuma.vote.domain.exception.OptionNumException;
-import com.kakao.golajuma.vote.infra.entity.Category;
 import com.kakao.golajuma.vote.infra.entity.VoteEntity;
 import com.kakao.golajuma.vote.infra.repository.OptionJPARepository;
 import com.kakao.golajuma.vote.infra.repository.VoteJPARepository;
@@ -21,29 +18,24 @@ public class CreateVoteService {
 	private final OptionJPARepository optionJPARepository;
 
 	@Transactional
-	public CreateVoteResponse createVote(CreateVoteRequest requestDto) {
-		boolean exit;
+	public CreateVoteResponse createVote(CreateVoteRequest requestDto, long userId) {
 		// 1. 투표 제목이 있는지 확인 후 예외처리
-		if (requestDto.getTitle() == null) {
-			throw new NullException("제목을 입력해주세요");
-		}
+		//		if (requestDto.getTitle() == null) {
+		//			throw new NullException("제목을 입력해주세요");
+		//		}
+		//
+		//		// 2. 옵션 개수가 2개 ~ 6개 인지 확인 후 예외처리
+		//		int size = requestDto.getOptions().size();
+		//		if (size < 2 || size > 6) {
+		//			throw new OptionNumException("선택지의 개수는 2개 이하 6개 이상이어야 합니다.");
+		//		}
+		//		// 3. 옵션 이름이 있는지 확인
+		//		for (CreateVoteRequest.OptionDTO option : requestDto.getOptions()) {
+		//			if (option.getName() == null) {
+		//				throw new NullException("옵션 이름을 입력해주세요.");
+		//			}
+		//		}
 
-		// 2. 옵션 개수가 2개 ~ 6개 인지 확인 후 예외처리
-		int size = requestDto.getOptions().size();
-		if (size < 2 || size > 6) {
-			throw new OptionNumException("선택지의 개수는 2개 이하 6개 이상이어야 합니다.");
-		}
-		// 3. 옵션 이름이 있는지 확인
-		for (CreateVoteRequest.OptionDTO option : requestDto.getOptions()) {
-			if (option.getName() == null) {
-				throw new NullException("옵션 이름을 입력해주세요.");
-			}
-		}
-
-		// 4. 카테고리가 맞는지 확인
-		Category.findCategory(requestDto.getCategory());
-
-		System.out.println(requestDto);
 		VoteEntity vote = voteJPARepository.save(requestDto.toEntity());
 		long voteId = vote.getId();
 		for (CreateVoteRequest.OptionDTO option : requestDto.getOptions()) {
