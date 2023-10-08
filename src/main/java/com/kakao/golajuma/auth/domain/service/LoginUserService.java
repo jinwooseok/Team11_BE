07c2ebe9990084febb6bcd6aafe3741c79bd22f4
kssumin <201566@jnu.ac.kr>
2023-10-09 00:46:50 +0900

    fix : security 파일 삭제

--- src/main/java/com/kakao/golajuma/auth/domain/service/LoginUserService.java
+++ src/main/java/com/kakao/golajuma/auth/domain/service/LoginUserService.java
@@ -10,7 +10,6 @@ import com.kakao.golajuma.auth.web.dto.converter.TokenConverter;
 import com.kakao.golajuma.auth.web.dto.request.LoginUserRequest;
 import com.kakao.golajuma.auth.web.dto.response.TokenResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +23,7 @@ public class LoginUserService {
 	private final TokenConverter tokenConverter;
 	private final TokenResolver tokenResolver;
 	private final TokenService tokenService;
-
+	private final Encoder encoder;
 
 	@Transactional
 	public TokenResponse execute(final LoginUserRequest request) {
@@ -52,6 +51,5 @@ public class LoginUserService {
 
 	private boolean matchPassword(final String requestPassword, final String password) {
 		return encoder.matches(requestPassword, password);
-
 	}
 }
