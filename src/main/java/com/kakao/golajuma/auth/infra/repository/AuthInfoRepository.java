dda6b44586b985d1fe12d6e84d264ef36353bbb3
kssumin <201566@jnu.ac.kr>
2023-10-13 00:57:33 +0900

    refactor : (#68) authInfoEntity 메서드명 변경

--- src/main/java/com/kakao/golajuma/auth/infra/repository/AuthInfoRepository.java
+++ src/main/java/com/kakao/golajuma/auth/infra/repository/AuthInfoRepository.java
@@ -1,10 +1,9 @@
 package com.kakao.golajuma.auth.infra.repository;
 
 import com.kakao.golajuma.auth.infra.entity.AuthInfoEntity;
-import com.kakao.golajuma.auth.infra.entity.UserEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthInfoRepository extends JpaRepository<AuthInfoEntity, Long> {
-	Optional<AuthInfoEntity> findByUserEntityAndToken(UserEntity userEntity, String token);
+	Optional<AuthInfoEntity> findByUserIdAndToken(Long userId, String token);
 }
