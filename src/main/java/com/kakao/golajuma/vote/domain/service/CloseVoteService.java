a3f775cdacae62dd0182df74793e910991536bfa
PHS00 <hyunsoo159487@naver.com>
2023-10-22 14:56:26 +0900

    feat : #112 투표 마감 기능 추가

---
+++ src/main/java/com/kakao/golajuma/vote/domain/service/CloseVoteService.java
@@ -0,0 +1,30 @@
+package com.kakao.golajuma.vote.domain.service;
+
+import com.kakao.golajuma.vote.domain.exception.CloseException;
+import com.kakao.golajuma.vote.domain.exception.NullException;
+import com.kakao.golajuma.vote.infra.entity.VoteEntity;
+import com.kakao.golajuma.vote.infra.repository.VoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class CloseVoteService {
+	private final VoteRepository voteRepository;
+
+	public void closeVote(long voteId, Long userId) {
+		VoteEntity vote =
+				voteRepository.findById(voteId).orElseThrow(() -> new NullException("해당 투표가 존재하지 않습니다."));
+		// 작성자가 아닌 경우 예외
+		if (!vote.isOwner(userId)) {
+			throw new CloseException("투표 작성자가 아닙니다.");
+		}
+		// 이미 마감된 경우 예외
+		if (vote.isOn()) {
+			vote.close();
+		}
+		throw new CloseException("이미 완료된 투표입니다.");
+	}
+}
