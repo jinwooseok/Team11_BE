33aed840c7872f097e731fd83e7c7ead39f454d4
PHS00 <hyunsoo159487@naver.com>
2023-10-22 16:59:41 +0900

    refactor : #114 sort enum 추가

---
+++ src/main/java/com/kakao/golajuma/vote/domain/service/Sort.java
@@ -0,0 +1,28 @@
+package com.kakao.golajuma.vote.domain.service;
+
+import com.kakao.golajuma.vote.domain.exception.NullException;
+import com.kakao.golajuma.vote.domain.exception.SortException;
+
+import java.util.Arrays;
+
+public enum Sort {
+	CURRENT("current"),
+	POPULAR("popular");
+
+	private final String sort;
+
+	Sort(String sort) {
+		this.sort = sort;
+	}
+
+	public String getSort() {
+		return this.sort;
+	}
+
+	public static Sort findSort(String sort) {
+		return Arrays.stream(Sort.values())
+				.filter(Category -> Category.getSort().equals(sort))
+				.findAny()
+				.orElseThrow(() -> new SortException("잘못된 정렬 방식입니다."));
+	}
+}
