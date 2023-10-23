33e99ae61b2a02419854ef17be9075e2028eabb1
PHS00 <hyunsoo159487@naver.com>
2023-10-24 01:30:26 +0900

    chore : 어노테이션 중복 제거

--- src/main/java/com/kakao/golajuma/vote/domain/service/CreateVoteService.java
+++ src/main/java/com/kakao/golajuma/vote/domain/service/CreateVoteService.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @RequiredArgsConstructor
 @Service
 public class CreateVoteService {
