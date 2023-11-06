9ef918dee0665769eb0977948016768b6fd38b97
kssumin <201566@jnu.ac.kr>
2023-11-07 00:47:37 +0900

    chore : authcontroller로 클래스명 변경

--- src/main/java/com/kakao/golajuma/auth/web/controller/AuthController.java
+++ src/main/java/com/kakao/golajuma/auth/web/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-public class LoginController {
+public class AuthController {
 	private final LoginUserService loginUserUseCase;
 
 	@PostMapping("/login")
