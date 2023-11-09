43b8b1b3b582bef504772ddc67493985e0e3082b
jinwooseok <jinus7949@naver.com>
2023-11-09 17:42:57 +0900

    refactor : (#184) api를 nickName > nickname 변경

--- src/main/java/com/kakao/golajuma/auth/web/dto/request/UpdateUserNickNameRequest.java
+++ src/main/java/com/kakao/golajuma/auth/web/dto/request/UpdateUserNickNameRequest.java
@@ -1,12 +1,14 @@
 package com.kakao.golajuma.auth.web.dto.request;
 
+import com.kakao.golajuma.auth.web.supplier.NicknameSupplier;
+import com.kakao.golajuma.common.marker.AbstractRequestDto;
 import javax.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
-public class UpdateUserNickNameRequest {
-	@NotBlank private String nickName;
+@Builder(toBuilder = true)
+public class UpdateUserNickNameRequest implements AbstractRequestDto, NicknameSupplier {
+	@NotBlank private String nickname;
 }
