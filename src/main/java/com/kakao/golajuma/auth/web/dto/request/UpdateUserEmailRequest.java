68e2df3fa2cc9b69465f5303e9996a27321d9685
jinwooseok <jinus7949@naver.com>
2023-11-09 17:42:27 +0900

    refactor : (#184) email 변경 request에 email유효성 검증 추가

--- src/main/java/com/kakao/golajuma/auth/web/dto/request/UpdateUserEmailRequest.java
+++ src/main/java/com/kakao/golajuma/auth/web/dto/request/UpdateUserEmailRequest.java
@@ -1,12 +1,15 @@
 package com.kakao.golajuma.auth.web.dto.request;
 
+import com.kakao.golajuma.auth.web.supplier.EmailSupplier;
+import com.kakao.golajuma.common.marker.AbstractRequestDto;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
-public class UpdateUserEmailRequest {
-	@NotBlank private String email;
+@Builder(toBuilder = true)
+public class UpdateUserEmailRequest implements AbstractRequestDto, EmailSupplier {
+	@NotBlank @Email private String email;
 }
