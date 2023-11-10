ca2ce0a8402d5332efbc7cdbc95d37489f75bcae
PHS00 <hyunsoo159487@naver.com>
2023-11-10 14:52:46 +0900

    refactor : #195 참여한 투표 리스트 조회 서비스 테스트 추가

---
+++ src/test/java/com/kakao/golajuma/vote/domain/service/GetParticipatedVotesServiceTest.java
@@ -0,0 +1,61 @@
+package com.kakao.golajuma.vote.domain.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.kakao.golajuma.vote.infra.entity.Category;
+import com.kakao.golajuma.vote.infra.entity.VoteEntity;
+import com.kakao.golajuma.vote.infra.repository.VoteRepository;
+import com.kakao.golajuma.vote.web.dto.response.GetVotesResponse;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class GetParticipatedVotesServiceTest {
+
+	@InjectMocks GetParticipatedVotesService getParticipatedVotesService;
+	@Mock VoteRepository voteRepository;
+
+	VoteEntity voteEntity;
+	List<VoteEntity> votes;
+
+	private void myPageNormalThen(GetVotesResponse.MyPage result) {
+		assertEquals("title", result.getVotes().get(0).getTitle());
+		assertEquals("complete", result.getVotes().get(0).getActive());
+		assertEquals(1, result.getVotes().size());
+	}
+
+	@DisplayName("마이페이지 참여한 투표 조회 정상 요청")
+	@Test
+	public void myPageParticipateTest() {
+		// given
+		Long userId = 1L;
+
+		// VoteEntity 생성
+		voteEntity =
+				VoteEntity.builder()
+						.id(1L)
+						.userId(1L)
+						.voteTitle("title")
+						.voteEndDate(LocalDateTime.now())
+						.category(Category.TOTAL)
+						.build();
+		votes = new ArrayList<>();
+		votes.add(voteEntity);
+
+		// when
+		when(voteRepository.findAllParticipateListByUserId(any())).thenReturn(votes);
+
+		GetVotesResponse.MyPage result = getParticipatedVotesService.execute(userId);
+		// then
+		myPageNormalThen(result);
+	}
+}
