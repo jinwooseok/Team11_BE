7bb0244db30886d479b10f24c0d7e28ee2f33666
kssumin <201566@jnu.ac.kr>
2023-11-11 16:11:21 +0900

    refactor : cookie에서 refreshToken 추출

---
+++ src/main/java/com/kakao/golajuma/auth/web/support/CookieExtractor.java
@@ -0,0 +1,43 @@
+package com.kakao.golajuma.auth.web.support;
+
+import com.kakao.golajuma.auth.domain.exception.CookieException;
+import java.util.Arrays;
+import java.util.Optional;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieExtractor implements TokenExtractor {
+	private static final String REFRESH_KEY = "refreshToken";
+
+	@Override
+	public String extract(HttpServletRequest request) {
+		Cookie[] cookies = getCookies(request);
+		Optional<Cookie> tokenCookie =
+				Arrays.stream(cookies).filter(cookie -> cookie.getName().equals(REFRESH_KEY)).findAny();
+		Cookie refreshCookie = tokenCookie.orElseThrow(CookieException::new);
+
+		return getValue(refreshCookie);
+	}
+
+	private Cookie[] getCookies(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		if (cookies == null) {
+			throw new CookieException();
+		}
+		return cookies;
+	}
+
+	private String getValue(Cookie cookie) {
+		String token = cookie.getValue();
+		validNullToken(token);
+		return token;
+	}
+
+	private void validNullToken(String token) {
+		if (token == null) {
+			throw new CookieException();
+		}
+	}
+}
