f1abf17c7bf6c777f01883711f8569db9993a4f2
jinwooseok <jinus7949@naver.com>
2023-11-11 11:56:50 +0900

    refactor : (#184) public 제거

--- src/test/java/com/kakao/golajuma/comment/service/GetCommentCountServiceTest.java
+++ src/test/java/com/kakao/golajuma/comment/service/GetCommentCountServiceTest.java
@@ -14,7 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class GetCommentCountServiceTest {
+class GetCommentCountServiceTest {
 
 	@InjectMocks private GetCommentCountService getCommentCountService;
 
