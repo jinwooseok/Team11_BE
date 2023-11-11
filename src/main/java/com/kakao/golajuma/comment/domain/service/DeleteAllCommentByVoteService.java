e2b23880770e0d04327ab6f71556aee508d99de7
kssumin <201566@jnu.ac.kr>
2023-11-11 15:20:19 +0900

    feat : user가 한 특정 vote에 대한 댓글 모두 삭제 구현

---
+++ src/main/java/com/kakao/golajuma/comment/domain/service/DeleteAllCommentByVoteService.java
@@ -0,0 +1,22 @@
+package com.kakao.golajuma.comment.domain.service;
+
+import com.kakao.golajuma.comment.infra.entity.CommentEntity;
+import com.kakao.golajuma.comment.infra.repository.CommentRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class DeleteAllCommentByVoteService {
+	private final CommentRepository commentRepository;
+
+	public void execute(Long voteId, Long userId) {
+		List<CommentEntity> comments = commentRepository.findAllByVoteIdAndUserId(voteId, userId);
+		commentRepository.deleteAll(comments);
+	}
+}
