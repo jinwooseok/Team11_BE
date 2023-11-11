3d439bc6eee46ef2c9e61576cf1cc969835fe4b1
kssumin <201566@jnu.ac.kr>
2023-11-11 11:41:26 +0900

    refactor : service 단 통일로 인한 분기

---
+++ src/main/java/com/kakao/golajuma/vote/domain/service/CreateDecisionService.java
@@ -0,0 +1,87 @@
+package com.kakao.golajuma.vote.domain.service;
+
+import com.kakao.golajuma.vote.domain.converter.DecisionEntityConverter;
+import com.kakao.golajuma.vote.domain.exception.decision.CompletionVoteException;
+import com.kakao.golajuma.vote.domain.exception.decision.ExistsDecisionException;
+import com.kakao.golajuma.vote.domain.exception.vote.NotFoundOptionException;
+import com.kakao.golajuma.vote.domain.exception.vote.NotFoundVoteException;
+import com.kakao.golajuma.vote.infra.entity.DecisionEntity;
+import com.kakao.golajuma.vote.infra.entity.OptionEntity;
+import com.kakao.golajuma.vote.infra.entity.VoteEntity;
+import com.kakao.golajuma.vote.infra.repository.DecisionRepository;
+import com.kakao.golajuma.vote.infra.repository.OptionRepository;
+import com.kakao.golajuma.vote.infra.repository.VoteRepository;
+import com.kakao.golajuma.vote.web.dto.converter.DecisionResponseConverter;
+import com.kakao.golajuma.vote.web.dto.response.DecisionResponse;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CreateDecisionService {
+	private final DecisionEntityConverter entityConverter;
+	private final DecisionRepository decisionRepository;
+	private final OptionRepository optionRepository;
+	private final VoteRepository voteRepository;
+	private final DecisionResponseConverter responseConverter;
+
+	public DecisionResponse execute(final Long userId, final Long optionId) {
+		VoteEntity voteEntity = findVote(optionId);
+		OptionEntity optionEntity = findOption(optionId);
+
+		boolean isExist = existDecisionByVote(userId, voteEntity.getId());
+		if (isExist) {
+			throw new ExistsDecisionException();
+		}
+
+		saveDecision(userId, voteEntity, optionEntity);
+
+		return generateResponse(voteEntity, optionId);
+	}
+
+	private boolean existDecisionByVote(Long userId, Long voteId) {
+		List<OptionEntity> options = optionRepository.findAllByVoteId(voteId);
+
+		return options.stream()
+				.map(option -> decisionRepository.findByUserIdAndOptionId(userId, option.getId()))
+				.anyMatch(Optional::isPresent);
+	}
+
+	private void saveDecision(
+			final Long userId, final VoteEntity voteEntity, final OptionEntity optionEntity) {
+		validateVoteStatus(voteEntity);
+
+		updateCounts(voteEntity, optionEntity);
+
+		DecisionEntity entity = entityConverter.from(userId, optionEntity.getId());
+		decisionRepository.save(entity);
+	}
+
+	private OptionEntity findOption(final Long optionId) {
+		return optionRepository.findById(optionId).orElseThrow(NotFoundOptionException::new);
+	}
+
+	private VoteEntity findVote(final Long optionId) {
+		return voteRepository.findVoteByOption(optionId).orElseThrow(NotFoundVoteException::new);
+	}
+
+	private void validateVoteStatus(final VoteEntity voteEntity) {
+		if (voteEntity.isComplete()) {
+			throw new CompletionVoteException();
+		}
+	}
+
+	private void updateCounts(final VoteEntity voteEntity, final OptionEntity optionEntity) {
+		voteEntity.updateCount();
+		optionEntity.updateCount();
+	}
+
+	private DecisionResponse generateResponse(final VoteEntity voteEntity, final Long selectId) {
+		List<OptionEntity> optionsByVote = optionRepository.findAllByVoteId(voteEntity.getId());
+		return responseConverter.from(selectId, optionsByVote, voteEntity.getVoteTotalCount());
+	}
+}
--- src/main/java/com/kakao/golajuma/vote/web/controller/DecisionController.java
+++ src/main/java/com/kakao/golajuma/vote/web/controller/DecisionController.java
@@ -5,7 +5,9 @@ import com.kakao.golajuma.common.support.respnose.ApiResponse;
 import com.kakao.golajuma.common.support.respnose.ApiResponseBody.SuccessBody;
 import com.kakao.golajuma.common.support.respnose.ApiResponseGenerator;
 import com.kakao.golajuma.common.support.respnose.MessageCode;
-import com.kakao.golajuma.vote.domain.service.DecisionService;
+import com.kakao.golajuma.vote.domain.service.CreateDecisionService;
+import com.kakao.golajuma.vote.domain.service.DeleteDecisionService;
+import com.kakao.golajuma.vote.domain.service.UpdateDecisionService;
 import com.kakao.golajuma.vote.web.dto.response.DecisionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,26 +22,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/votes/participate")
 public class DecisionController {
-	private final DecisionService decisionService;
+	private final CreateDecisionService createDecisionService;
+	private final UpdateDecisionService updateDecisionService;
+	private final DeleteDecisionService deleteDecisionService;
 
 	@PostMapping("/options/{optionId}")
 	public ApiResponse<SuccessBody<DecisionResponse>> createVote(
 			@PathVariable Long optionId, @Login Long userId) {
-		DecisionResponse response = decisionService.createDecision(userId, optionId);
+		DecisionResponse response = createDecisionService.execute(userId, optionId);
 		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.CREATE);
 	}
 
 	@DeleteMapping("/options/{optionId}")
 	public ApiResponse<SuccessBody<DecisionResponse>> deleteVote(
 			@PathVariable Long optionId, @Login Long userId) {
-		DecisionResponse response = decisionService.deleteVote(userId, optionId);
+		DecisionResponse response = deleteDecisionService.execute(userId, optionId);
 		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.DELETE);
 	}
 
 	@PutMapping("/options/{optionId}")
 	public ApiResponse<SuccessBody<DecisionResponse>> updateVote(
 			@PathVariable Long optionId, @Login Long userId) {
-		DecisionResponse response = decisionService.updateVote(userId, optionId);
+		DecisionResponse response = updateDecisionService.execute(userId, optionId);
 		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.UPDATE);
 	}
 }
