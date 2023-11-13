2db2e610ccba5a65c3f6b883ecdc36e4ee42c5de
jinwooseok <jinus7949@naver.com>
2023-11-14 01:23:45 +0900

    release : 11.13 23:48 빌드

--- k8s/mysql-init.d/00_init.sql
+++ k8s/mysql-init.d/00_init.sql
@@ -1,6 +1,12 @@
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON krampoline.* TO 'root'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE
+USER 'root'@'localhost' IDENTIFIED BY 'root';
+CREATE
+USER 'root'@'%' IDENTIFIED BY 'root';
+
+GRANT ALL PRIVILEGES ON *.* TO
+'root'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO
+'root'@'%';
 
 CREATE
-    DATABASE krampoline DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+DATABASE krampoline DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
